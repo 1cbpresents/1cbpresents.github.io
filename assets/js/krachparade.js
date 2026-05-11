834c125420ad6416f37b36459f6f84af048a08b6
Nico Baumeister <nico.baumeister@adac.de>
2026-05-11 08:30:17 +0200

    created krachparade js test

--- assets/js/krachparade.js
+++ assets/js/krachparade.js
@@ -1,8 +1,63 @@
 (function () {
   'use strict';
 
-  const CURRENT_DJ_ID = 'neon-mara';
+  const CURRENT_DJ_ID = 'basstian-null';
   const DONATION_URL = 'https://paypal.me/DEINLINK';
+
+  const DJS = [
+    {
+      id: 'neon-mara',
+      name: 'Neon Mara',
+      instagram: '@neon.mara',
+      instagramUrl: 'https://www.instagram.com/neon.mara/',
+      time: '14:00 - 15:00',
+      motto: 'Chrome kicks, open-air pressure.',
+      image: '/assets/images/krachparade/djs/neon-mara.webp'
+    },
+    {
+      id: 'basstian-null',
+      name: 'Basstian Null',
+      instagram: '@basstian.null',
+      instagramUrl: 'https://www.instagram.com/basstian.null/',
+      time: '15:00 - 16:00',
+      motto: 'Industrial bounce for moving streets.',
+      image: '/assets/images/krachparade/djs/basstian-null.webp'
+    },
+    {
+      id: 'luna-kick',
+      name: 'Luna Kick',
+      instagram: '@luna.kick',
+      instagramUrl: 'https://www.instagram.com/luna.kick/',
+      time: '16:00 - 17:00',
+      motto: 'Fast smiles, heavy subs.',
+      image: '/assets/images/krachparade/djs/luna-kick.webp'
+    },
+    {
+      id: 'rave-kasimir',
+      name: 'Rave Kasimir',
+      instagram: '@rave.kasimir',
+      instagramUrl: 'https://www.instagram.com/rave.kasimir/',
+      time: '17:00 - 18:00',
+      motto: 'Peak-time dust and parade sirens.',
+      image: '/assets/images/krachparade/djs/rave-kasimir.webp'
+    },
+    {
+      id: 'toni-triebwerk',
+      name: 'Toni Triebwerk',
+      instagram: '@toni.triebwerk',
+      instagramUrl: 'https://www.instagram.com/toni.triebwerk/',
+      time: '18:00 - 19:00',
+      motto: 'Final gear, full collective lift-off.',
+      image: '/assets/images/krachparade/djs/toni-triebwerk.webp'
+    }
+  ];
+
+  const WAVEFORM_PATTERN = [
+    18, 42, 28, 58, 34, 68, 44, 24,
+    52, 36, 64, 30, 48, 70, 26, 56,
+    38, 62, 22, 46, 66, 32, 54, 40,
+    72, 28, 60, 36, 50, 20, 58, 44
+  ];
 
   const selectors = {
     player: '.now-player',
@@ -26,50 +81,14 @@
     return params.get('dj');
   }
 
-  function textFrom(parent, selector) {
-    const element = parent.querySelector(selector);
-    return element ? element.textContent.trim() : '';
-  }
-
-  function readDjFromItem(item, index) {
-    const data = item.dataset || {};
-
-    return {
-      id: data.djId || '',
-      name: data.name || textFrom(item, '.timetable__artist strong'),
-      instagram: data.instagram || textFrom(item, '.timetable__artist span'),
-      instagramUrl: data.instagramUrl || 'https://www.instagram.com/1cb__kollektiv/',
-      time: data.time || textFrom(item, '.timetable__time'),
-      motto: data.motto || '',
-      image: data.image || '',
-      item: item,
-      index: index
-    };
-  }
-
-  function getDjs() {
-    const timetable = getElement(selectors.timetable);
-
-    if (!timetable) {
-      return [];
-    }
-
-    return Array.prototype.slice
-      .call(timetable.querySelectorAll('.timetable__item'))
-      .map(readDjFromItem)
-      .filter(function (dj) {
-        return dj.id && dj.name;
-      });
-  }
-
-  function findDj(djs, id) {
-    return djs.find(function (dj) {
+  function findDj(id) {
+    return DJS.find(function (dj) {
       return dj.id === id;
     });
   }
 
-  function getActiveDj(djs) {
-    return findDj(djs, getRequestedDjId()) || findDj(djs, CURRENT_DJ_ID) || djs[0];
+  function getActiveDj() {
+    return findDj(getRequestedDjId()) || findDj(CURRENT_DJ_ID) || DJS[0];
   }
 
   function setText(selector, value) {
@@ -94,24 +113,49 @@
 
     waveform.textContent = '';
 
-    for (let index = 0; index < 24; index += 1) {
+    WAVEFORM_PATTERN.forEach(function (height, index) {
       const bar = document.createElement('span');
-      const height = 22 + (((index * 17) + (activeIndex * 11)) % 48);
-      bar.style.height = height + 'px';
-      bar.style.animationDelay = (index * -0.075) + 's';
+      const shiftedHeight = WAVEFORM_PATTERN[(index + (activeIndex * 3)) % WAVEFORM_PATTERN.length];
+      bar.style.height = shiftedHeight + 'px';
+      bar.style.animationDelay = (index * -0.055) + 's';
+      bar.style.animationDuration = (0.8 + ((index % 5) * 0.08)) + 's';
       waveform.appendChild(bar);
-    }
+    });
   }
 
-  function renderTimetable(djs, activeDj) {
-    djs.forEach(function (dj) {
-      dj.item.classList.remove('is-active');
-      dj.item.removeAttribute('aria-current');
+  function renderTimetable(activeDj) {
+    const timetable = getElement(selectors.timetable);
+    if (!timetable) {
+      return;
+    }
+
+    timetable.textContent = '';
+
+    DJS.forEach(function (dj) {
+      const item = document.createElement('li');
+      item.className = 'timetable__item';
 
       if (dj.id === activeDj.id) {
-        dj.item.classList.add('is-active');
-        dj.item.setAttribute('aria-current', 'true');
+        item.classList.add('is-active');
+        item.setAttribute('aria-current', 'true');
       }
+
+      const time = document.createElement('span');
+      time.className = 'timetable__time';
+      time.textContent = dj.time;
+
+      const artist = document.createElement('span');
+      artist.className = 'timetable__artist';
+
+      const name = document.createElement('strong');
+      name.textContent = dj.name;
+
+      const instagram = document.createElement('span');
+      instagram.textContent = dj.instagram;
+
+      artist.append(name, instagram);
+      item.append(time, artist);
+      timetable.appendChild(item);
     });
   }
 
@@ -143,20 +187,17 @@
   }
 
   function renderActiveDj() {
-    const djs = getDjs();
-
-    if (!djs.length) {
-      return;
-    }
-
-    const activeDj = getActiveDj(djs);
+    const activeDj = getActiveDj();
+    const activeIndex = DJS.findIndex(function (dj) {
+      return dj.id === activeDj.id;
+    });
 
     document.title = '1CB Krachparade - ' + activeDj.name;
 
     setText(selectors.name, activeDj.name);
     setText(selectors.motto, activeDj.motto);
     setText(selectors.time, activeDj.time);
-    setText(selectors.position, 'Set ' + String(activeDj.index + 1).padStart(2, '0'));
+    setText(selectors.position, 'Set ' + String(activeIndex + 1).padStart(2, '0'));
     setLink(selectors.donationLink, DONATION_URL);
 
     const instagramLink = getElement(selectors.instagramLink);
@@ -167,8 +208,8 @@
     }
 
     renderImage(activeDj);
-    renderWaveform(activeDj.index);
-    renderTimetable(djs, activeDj);
+    renderWaveform(activeIndex);
+    renderTimetable(activeDj);
   }
 
   if (document.readyState === 'loading') {
